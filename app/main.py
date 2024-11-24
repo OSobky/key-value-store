ff4ecbf12beeb38e7616f3f4c6cd0b304b07ee30
OSobky <omarelsobky97@gmail.com>
2024-11-25 00:02:06 +0100

    update the CURD operations to handle GET, DELETE, POST Key-Value pairs

--- app/main.py
+++ app/main.py
@@ -1,20 +1,43 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Response, status
+from pydantic import BaseModel
+
+# Create Key-Value Pair
+class keyValue(BaseModel):
+    key: str
+    value: str
 
 app = FastAPI()
 
+# In-memory store
+store = {}
 
 @app.get("/")
 async def root():
-    return {"message": "Hello World"}
+    return {"message": "Welcome to the Key-Value Store. Make sure to check the docs at /docs to see what you can do."}
 
-@app.get("/value")
-async def value():
-    return {"value": "Looking for the key to get the value"}
+@app.get("/value/{key}", status_code=status.HTTP_200_OK)
+async def getValue(key: str, response: Response):
+    if key in store:
+        return {"value": store[key]}
+    else:
+        response.status_code = status.HTTP_404_NOT_FOUND
+        return {"message": "Key not found."}
 
-@app.post("/value")
-async def value():
-    return {"value": "Did not post yet looking for the key"}
 
-@app.delete("/value")
-async def value():
-    return {"value": "Did not delete yet looking for the key"}
+@app.post("/value/", status_code=status.HTTP_201_CREATED)
+async def addKeyValue(keyvalue: keyValue, response: Response):
+    if keyvalue.key not in store:
+        store[keyvalue.key] = keyvalue.value
+        return {"message": "Key-Value pair added"}
+    else:
+        response.status_code = status.HTTP_409_CONFLICT
+        return {"message": "Key already exists. Stay tuned for the PUT method to update the value."}
+
+@app.delete("/value/{key}", status_code=status.HTTP_200_OK)
+async def deleteKeyValue(key: str, response: Response):
+    if key in store:
+        del store[key]
+        return {"message": "Key-Value pair deleted"}
+    else:
+        response.status_code = status.HTTP_404_NOT_FOUND
+        return {"message": "Key not found"}
